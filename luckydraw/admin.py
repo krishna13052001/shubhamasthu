49f46f0209229a1adea1b9fc4356249b959a65a7
AVS18 <avsadityavardhan18bcs@iiitkottayam.ac.in>
2021-12-30 20:26:49 +0530

    added winner admin.py

--- luckydraw/admin.py
+++ luckydraw/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cards,Coupon,CouponCount
+from .models import Cards,Coupon,CouponCount,Winner
 
 # Register your models here.
 class CouponRef(admin.ModelAdmin):
@@ -10,7 +10,16 @@ class CardRef(admin.ModelAdmin):
     list_filter = ['code','scratched','redeemed','redeemed_date']
 class CouponCountRef(admin.ModelAdmin):
     list_display = ['tirupati_count','nellore_count']
+class WinnerRef(admin.ModelAdmin):
+    list_display = ('winner_code','winner_coupon_name','winner_coupon_mobile')
+    def winner_code(self, instance):
+        return instance.winner_card.code
+    def winner_coupon_name(self, instance):
+        return instance.winner_coupon.name
+    def winner_coupon_mobile(self, instance):
+        return instance.winner_coupon.mobile
 #link models to admin dashboard
 admin.site.register(Coupon,CouponRef)
 admin.site.register(Cards,CardRef)
 admin.site.register(CouponCount,CouponCountRef)
+admin.site.register(Winner,WinnerRef)
