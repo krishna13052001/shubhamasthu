0c1d2482a5c74fe922f67657fa36ceb749b39da7
AVS18 <avsadityavardhan18bcs@iiitkottayam.ac.in>
2022-01-01 01:50:05 +0530

    added changes

--- scard/settings.py
+++ scard/settings.py
@@ -28,7 +28,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = True
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('HEROKU_POSTGRESQL_YELLOW_URL')
+        default=config('PAID_DBURL')
     )
 }
 '''
